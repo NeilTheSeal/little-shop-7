bd47dede33e83754ddcf6c004edaa2a828ce56d5
Grant Davis <grantdavis303@gmail.com>
2024-04-10 08:55:00 -0600

    Add Welcome Page and Routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   root "welcome#index"
 
   # resources :dashboard, as: "merchant_dashboard"
-
   resources :merchants do
     member { get "dashboard"}
     resources :items, controller: "merchant_items", only: [:index]
