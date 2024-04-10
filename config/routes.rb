9f0fe712f498b9909e658dbe8b568b1e58f1d2a4
Luis Aparicio <lfelipeaparicio2004@gmail.com>
2024-04-09 20:29:28 -0400

    Finish user story 2

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   # root "articles#index"
   # resources :dashboard, as: "merchant_dashboard"
   resources :merchants do
-
     member { get "dashboard"}
+    resources :items, controller: "merchant_items", only: [:index]
+    resources :invoices, controller: "merchant_invoices", only: [:index]
   end
   # get "/merchants/:id/dashboard", to: "merchants#dashboard"
 end
--- spec/features/merchants/dashboard_spec.rb
+++ spec/features/merchants/dashboard_spec.rb
@@ -17,19 +17,22 @@ RSpec.describe 'merchant dashboard', type: :feature do
   end
 
   # 2. Merchant Dashboard Links
-  it "" do
+  it "has links to merchant items and invoices" do
     # As a merchant,
     # When I visit my merchant dashboard (/merchants/:merchant_id/dashboard)
     visit dashboard_merchant_path(@merchant)
     # Then I see link to my merchant items index (/merchants/:merchant_id/items)
     within '.merchant_items' do
-      expect(page).to have_link("Merchant")
-      click_link()
-      expect(current_path).to eq()
+      expect(page).to have_link("Merchant Items")
+      click_link("Merchant Items")
+      expect(current_path).to eq(merchant_items_path(@merchant))
     end
+    visit dashboard_merchant_path(@merchant)
     # And I see a link to my merchant invoices index (/merchants/:merchant_id/invoices)
-    within '.' do
-      
+    within '.merchant_invoices' do
+      expect(page).to have_link("Merchant Invoices")
+      click_link("Merchant Invoices")
+      expect(current_path).to eq(merchant_invoices_path(@merchant))
     end
   end
 end
