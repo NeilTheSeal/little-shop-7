bd1fe5cbf349ee31374b1ae5f082bab546630d2d
Aurora Sanderson <AuroraTuring@gmail.com>
2024-04-10 21:12:09 -0600

    Progress through user story 21

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,4 +4,19 @@ class Customer < ApplicationRecord
   has_many :items, through: :invoice_items
   has_many :transactions, through: :invoices
   has_many :merchants, through: :items
+
+  def top_customer_id
+    # Customer.joins(:transactions)
+      # .where("transactions.result='success'")
+      # .order("count_all DESC")
+      # .limit(5)
+      # .group("customers.id").count
+    Customer.select("customers.first_name, customers.last_name, customers.id, COUNT(customers.id)")
+      .joins(:transactions)
+      .where("transactions.result = 'success'")
+      .group("customers.id")
+      .order("COUNT(customers.id) DESC")
+      .limit(5)
+  end
+
 end
---
+++ spec/models/customer_spec.rb
@@ -0,0 +1,51 @@
+require("rails_helper")
+
+RSpec.describe Customer do
+  before(:each) do
+    @customer = create(:customer)
+
+    @merchant = create(:merchant)
+
+    @invoice = create(:invoice, customer: @customer)
+    @invoice_list = create_list(:invoice, 5, customer: @customer)
+
+    @item = create(:item, merchant: @merchant)
+    @item_list = create_list(:item, 5, merchant: @merchant)
+
+    @invoice_item = create(
+      :invoice_item,
+      item: @item,
+      invoice: @invoice,
+      unit_price: @item.unit_price
+    )
+
+    @invoice_items_list = create_list(
+      :invoice_item,
+      5,
+      item: @item_list[0],
+      invoice: @invoice_list[0],
+      unit_price: @item_list[0].unit_price
+    )
+
+    @transaction = create(:transaction, invoice: @invoice_list[0])
+  end
+
+  describe "associations" do
+    it { should have_many :invoices }
+    it { should have_many(:invoice_items).through(:invoices) }
+  end
+
+end
+
+
+
+# it "examples" do
+  # p @customer
+  # p @merchant
+  # p @invoice
+  # p @invoice_list[0]
+  # p @item
+  # p @item_list[0]
+  # p @invoice_item
+  # p @invoice_items_list[0]
+  # p @transaction
