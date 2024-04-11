e92f6b2508fc1523c6bf072d327883110f65d53a
Luis Aparicio <lfelipeaparicio2004@gmail.com>
2024-04-11 13:59:22 -0400

    Implement top five customers method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,14 +2,13 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :invoice_items, through: :items
   has_many :invoices, through: :invoice_items
-  has_many :customers, through: :invoices
   has_many :transactions, through: :invoices
+  has_many :customers, through: :invoices
 
-  def top_5_customers
-    self.transactions
-      .joins(invoice: :customer)
+  def top_five_customers
+    transactions.joins(invoice: :customer)
       .where("transactions.result = '1'")      
-      .select("customer.id, CONCAT(customers.first_name, '', customers.last_name) AS customer_name, COUNT(transactions.result) AS transaction_result")
+      .select("customers.id, CONCAT(customers.first_name, ' ', customers.last_name) AS customer_name, COUNT(transactions.result) AS transaction_result")
       .group("customers.id")
       .order("transaction_result DESC")
       .limit(5)
