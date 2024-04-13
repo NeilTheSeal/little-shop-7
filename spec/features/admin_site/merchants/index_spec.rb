88e45cee3477c6df6269226981c6497a62575e31
NeilTheSeal <neil.hendren@gmail.com>
2024-04-12 18:54:34 -0600

    Implement test for user story 28

--- spec/features/admin_site/merchants/index_spec.rb
+++ spec/features/admin_site/merchants/index_spec.rb
@@ -3,6 +3,7 @@ require("rails_helper")
 RSpec.describe "Merchant dashboard" do
   before(:each) do
     @merchants_list = create_list(:merchant, 3)
+    @disabled_merchant = create(:merchant, status: 1)
   end
 
   it "shows the name of each merchant in the system and their status" do
@@ -42,6 +43,18 @@ RSpec.describe "Merchant dashboard" do
 
     within "#merchant-#{@merchants_list[0].id}" do
       expect(page).to have_button("disable")
+    end
+  end
+
+  it "has a column for enabled merchants" do
+    visit "/admin/merchants"
+
+    within "#enabled-merchants" do
+      expect(page).to have_content(@merchants_list[0].name)
+    end
+
+    within "#disabled-merchants" do
+      expect(page).to have_content(@disabled_merchant.name)
     end
   end
 end
