b27aaaec75c2f31a18fe0a4213fb0372c9914d88
Grant Davis <grantdavis303@gmail.com>
2024-04-12 23:21:19 -0600

    Create Edit Form

--- app/controllers/merchant_site/items_controller.rb
+++ app/controllers/merchant_site/items_controller.rb
@@ -8,5 +8,24 @@ module MerchantSite
       @merchant = Merchant.find(params[:merchant_id])
       @item = Item.find(params[:id])
     end
+
+    def edit
+      @merchant = Merchant.find(params[:merchant_id])
+      @item = Item.find(params[:id]) 
+    end
+
+    def update
+      @merchant = Merchant.find(params[:merchant_id])
+      @item = Item.find(params[:id])
+      @item.update(item_params)
+
+      redirect_to merchant_item_path(@merchant, @item)
+    end
+
+    private
+    # Strong Params
+    def item_params
+      params.permit(:name, :description, :unit_price)
+    end
   end
 end
