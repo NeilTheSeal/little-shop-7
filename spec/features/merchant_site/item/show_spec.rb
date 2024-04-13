9e2538bc0222158d1990d13168f81603c2dcbb79
Grant Davis <grantdavis303@gmail.com>
2024-04-12 23:21:00 -0600

    Update US 8

--- spec/features/merchant_site/item/show_spec.rb
+++ spec/features/merchant_site/item/show_spec.rb
@@ -56,4 +56,25 @@ RSpec.describe 'Merchant_items#show', type: :feature do
     end
   end
 
+  # User Story 8
+  xit 'update item button' do
+    # As a merchant,when I visit the merchant show page of an item (/merchants/:merchant_id/items/:item_id)
+    visit merchant_items_path(@merchant, @item_list[0])
+    # I see a link to update the item information.
+    expect(page).to have_button("Update Item")
+    # When I click the link
+    click_link "Update Item"
+    # Then I am taken to a page to edit this item
+    expect(current_path).to eq(edit_merchant_item_path(@merchant, @item_list[0]))
+    # And I see a form filled in with the existing item attribute information
+    # When I update the information in the form and I click ‘submit’
+
+    fill_in 
+    click_button "Update Item"
+    
+    # Then I am redirected back to the item show page where I see the updated information
+    expect(current_path).to eq(merchant_item_path(@merchant, @item_list[0]))
+    # And I see a flash message stating that the information has been successfully updated.
+  end
+
 end
