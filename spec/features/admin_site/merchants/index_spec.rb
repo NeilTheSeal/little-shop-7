4c237b4d6e3c66683e403d08b65d09c7bdbfb433
NeilTheSeal <neil.hendren@gmail.com>
2024-04-14 20:29:32 -0600

    Write tests for user story 30

--- spec/features/admin_site/merchants/index_spec.rb
+++ spec/features/admin_site/merchants/index_spec.rb
@@ -4,6 +4,199 @@ RSpec.describe "Merchant dashboard" do
   before(:each) do
     @merchants_list = create_list(:merchant, 3)
     @disabled_merchant = create(:merchant, status: 1)
+    @customers = create_list(:customer, 5)
+    @merchants = create_list(:merchant, 6)
+    @dummy_customer = create(:customer)
+    @failure_customer = create(:customer)
+
+    # merchants[0] = 3000
+    # merchants[1] = 4000
+    # merchants[2] = 5000
+    # merchants[3] = 6000
+    # merchants[4] = 7000
+    # merchants[5] = 8000
+
+    @item_list1 = create_list(
+      :item,
+      3,
+      merchant: @merchants[0],
+      unit_price: 1000
+    )
+    @item_list2 = create_list(
+      :item,
+      3,
+      merchant: @merchants[1],
+      unit_price: 1000
+    )
+    @item_list3 = create_list(
+      :item,
+      3,
+      merchant: @merchants[2],
+      unit_price: 1000
+    )
+    @item_list4 = create_list(
+      :item,
+      3,
+      merchant: @merchants[3],
+      unit_price: 1000
+    )
+    @item_list5 = create_list(
+      :item,
+      3,
+      merchant: @merchants[4],
+      unit_price: 1000
+    )
+    @item_list6 = create_list(
+      :item,
+      3,
+      merchant: @merchants[5],
+      unit_price: 1000
+    )
+
+    @customer1_invoice_list = create_list(
+      :invoice,
+      1,
+      customer: @customers[0],
+      created_at: "2020-01-01 00:00:00"
+    )
+    @customer2_invoice_list = create_list(
+      :invoice,
+      3,
+      customer: @customers[1],
+      created_at: "2022-01-01 00:00:00"
+    )
+    @customer3_invoice_list = create_list(
+      :invoice,
+      5,
+      customer: @customers[2],
+      created_at: "2021-01-01 00:00:00"
+    )
+    @customer4_invoice_list = create_list(
+      :invoice,
+      4,
+      customer: @customers[3]
+    )
+    @customer5_invoice_list = create_list(
+      :invoice,
+      2,
+      customer: @customers[4]
+    )
+    @dummy_invoice = create(:invoice, customer: @dummy_customer)
+    @failure_invoice = create(:invoice, customer: @failure_customer)
+
+    @invoice_item1 = create(
+      :invoice_item,
+      item: @item_list1[0],
+      invoice: @customer1_invoice_list[0],
+      unit_price: @item_list1[0].unit_price,
+      quantity: 1,
+      status: "shipped"
+    )
+    @invoice_item2 = create(
+      :invoice_item,
+      item: @item_list1[1],
+      invoice: @customer1_invoice_list[0],
+      unit_price: @item_list1[1].unit_price,
+      quantity: 1,
+      status: "shipped"
+    )
+    @invoice_item3 = create(
+      :invoice_item,
+      item: @item_list1[2],
+      invoice: @customer1_invoice_list[0],
+      unit_price: @item_list1[2].unit_price,
+      quantity: 1,
+      status: "shipped"
+    )
+    @invoice_item4 = create(
+      :invoice_item,
+      item: @item_list2[0],
+      invoice: @customer2_invoice_list[0],
+      unit_price: @item_list2[0].unit_price,
+      quantity: 1,
+      status: "shipped"
+    )
+    @invoice_item5 = create(
+      :invoice_item,
+      item: @item_list2[1],
+      invoice: @customer2_invoice_list[0],
+      unit_price: @item_list2[1].unit_price,
+      quantity: 1,
+      status: "pending"
+    )
+    @invoice_item6 = create(
+      :invoice_item,
+      item: @item_list2[2],
+      invoice: @customer2_invoice_list[0],
+      unit_price: @item_list2[2].unit_price,
+      quantity: 2,
+      status: "packaged"
+    )
+    @invoice_item7 = create(
+      :invoice_item,
+      item: @item_list3[0],
+      invoice: @customer3_invoice_list[0],
+      unit_price: @item_list3[0].unit_price,
+      quantity: 1,
+      status: "pending"
+    )
+    @invoice_item8 = create(
+      :invoice_item,
+      item: @item_list3[1],
+      invoice: @customer3_invoice_list[0],
+      unit_price: @item_list3[1].unit_price,
+      quantity: 2,
+      status: "packaged"
+    )
+    @invoice_item9 = create(
+      :invoice_item,
+      item: @item_list3[2],
+      invoice: @customer3_invoice_list[0],
+      unit_price: @item_list3[2].unit_price,
+      quantity: 2,
+      status: "packaged"
+    )
+    @invoice_item10 = create(
+      :invoice_item,
+      item: @item_list4[0],
+      invoice: @dummy_invoice,
+      unit_price: @item_list4[0].unit_price,
+      quantity: 6,
+      status: "packaged"
+    )
+    @invoice_item11 = create(
+      :invoice_item,
+      item: @item_list5[0],
+      invoice: @dummy_invoice,
+      unit_price: @item_list5[0].unit_price,
+      quantity: 7,
+      status: "packaged"
+    )
+    @invoice_item12 = create(
+      :invoice_item,
+      item: @item_list6[0],
+      invoice: @dummy_invoice,
+      unit_price: @item_list6[0].unit_price,
+      quantity: 8,
+      status: "packaged"
+    )
+    @invoice_item13 = create(
+      :invoice_item,
+      item: @item_list1[0],
+      invoice: @failure_invoice,
+      unit_price: @item_list1[0].unit_price,
+      quantity: 100,
+      status: "packaged"
+    )
+
+    @customer1_invoice_list.each { |invoice| create(:transaction, invoice:) }
+    @customer2_invoice_list.each { |invoice| create(:transaction, invoice:) }
+    @customer3_invoice_list.each { |invoice| create(:transaction, invoice:) }
+    @customer4_invoice_list.each { |invoice| create(:transaction, invoice:) }
+    @customer5_invoice_list.each { |invoice| create(:transaction, invoice:) }
+    create(:transaction, invoice: @dummy_invoice, result: "failed")
+    create(:transaction, invoice: @dummy_invoice, result: "success")
+    create(:transaction, invoice: @failure_invoice, result: "failed")
   end
 
   it "shows the name of each merchant in the system and their status" do
@@ -64,5 +257,26 @@ RSpec.describe "Merchant dashboard" do
     click_link("New Merchant")
 
     expect(page).to have_current_path("/admin/merchants/new")
+  end
+
+  it "shows the top 5 merchants" do
+    visit "/admin/merchants"
+
+    within "#top-merchants" do
+      expect(@merchants[5].name).to appear_before(@merchants[4].name)
+      expect(@merchants[4].name).to appear_before(@merchants[3].name)
+      expect(@merchants[3].name).to appear_before(@merchants[2].name)
+      expect(@merchants[2].name).to appear_before(@merchants[1].name)
+      expect(page).to_not have_content(@merchants[0].name)
+    end
+  end
+
+  it "has links to the show pages of the top 5 merchants" do
+    visit "/admin/merchants"
+
+    within "#top-merchants" do
+      click_link(@merchants[5].name)
+      expect(page).to have_current_path("/admin/merchants/#{@merchants[5].id}")
+    end
   end
 end
